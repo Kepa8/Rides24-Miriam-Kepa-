b57a7f43be37c71bc33106c440e5a7a4199d2420
ehu <ehu@U032831>
2024-10-02 12:19:19 +0200

    prueba ondo dago

--- src/test/java/CreateRideBDBlackTest.java
+++ src/test/java/CreateRideBDBlackTest.java
@@ -22,7 +22,7 @@ public class CreateRideBDBlackTest {
 
 	 //sut:system under test
 	 static DataAccess sut=new DataAccess();
-	 //dfjsdk
+	 //beste komentario
 	 
 	 //additional operations needed to execute the test 
 	 static TestDataAccess testDA=new TestDataAccess();
